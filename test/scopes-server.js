fef201a43522e0d116ddf932d4deaec713750a5d
Sebastian Sassi <ssassi@sophilabs.com>
2015-03-22 17:08:29 -0300

    unit tests validating scopes using new optional credentials argument

---
+++ test/scopes-server.js
@@ -0,0 +1,58 @@
+var Hapi   = require('hapi');
+var JWT    = require('jsonwebtoken');
+var secret = 'NeverShareYourSecret';
+
+// for debug options see: http://hapijs.com/tutorials/logging
+var server = new Hapi.Server({ debug: false })
+server.connection();
+
+var db = {
+  "123" : { allowed: true,  "name":"Charlie"  },
+  "321" : { allowed: false, "name":"Old Gregg"}
+ }
+
+var scopesDb = {
+  "123": ['Admin', 'Authenticated'],
+  "321": ['Authenticated']
+}
+
+// defining our own validate function lets us do something
+// useful/custom with the decodedToken before reply(ing)
+var validate = function (decoded, request, callback) {
+
+  // console.log(" - - - - - - - decoded token:");
+  // console.log(decoded);
+  // console.log(" - - - - - - - request info:");
+  // console.log(request.info);
+  // console.log(" - - - - - - - user agent: " + request.headers['user-agent']);
+
+    if (db[decoded.id].allowed) {
+      var credentials = db[decoded.id];
+      credentials.scope = scopesDb[decoded.id];
+      return callback(null, true, credentials);
+    }
+    else {
+      return callback(null, false);
+    }
+};
+
+var home = function(req, reply) {
+    reply('Hai!');
+}
+
+var privado = function(req, reply) {
+  reply('worked');
+}
+
+server.register(require('../'), function (err) {
+
+  server.auth.strategy('jwt', 'jwt', { key: secret,  validateFunc: validate });
+
+  server.route([
+    { method: 'GET',  path: '/', handler: home, config:{ auth: false } },
+    { method: 'POST', path: '/privado-with-scope', handler: privado, config: { auth: { strategy: 'jwt', scope: [ 'Admin' ] } } }
+  ]);
+
+});
+
+module.exports = server;
