d44bf4bac095f2e9d3bc34dc7b3e18bfdb8bca8e
nelsonic <contact.nelsonic+github@gmail.com>
2015-12-18 13:12:45 +0000

    adds ability to bypass JWT.verify as discussed in https://github.com/dwyl/hapi-auth-jwt2/issues/130

--- test/verify_bypass_server.js
+++ test/verify_bypass_server.js
@@ -5,47 +5,25 @@ var secret = 'NeverShareYourSecret';
 var server = new Hapi.Server({ debug: { 'request': ['error', 'uncaught'] } });
 server.connection();
 
-// defining our own validate function lets us do something
-// useful/custom with the decodedToken before reply(ing)
-var validate = function (decoded, request, callback) {
-  // don't need to add anything here
-};
-
-// see discussion in https://github.com/dwyl/hapi-auth-jwt2/issues/130
-// var bypass_validation = function(decoded, callback) {
-//   console.log(' - - - - - - - - - - - - - - - - > hello');
-//   console.log(decoded);
-//   // console.log(req);
-//   // can we simply short-circuit the verification?
-//   return reply.continue({ credentials: decoded});
-// }
-
 var sendToken = function(req, reply) {
   return reply(req.auth.token);
 };
 
-var home = function(req, reply) {
-  return reply('Hai!');
-};
-
 var privado = function(req, reply) {
-  return reply('worked');
+
+  return reply(req.auth.credentials);
 };
 
 server.register(require('../'), function () {
 
   server.auth.strategy('jwt', 'jwt', {
-    key: 'bypass_validation',
-    validateFunc: validate,
-    verifyOptions: { algorithms: [ 'HS256' ] } // only allow HS256 algorithm
+    bypass_verifcation: true // no validateFunc or key required.
   });
 
   server.route([
-    { method: 'GET',  path: '/', handler: home, config: { auth: false } },
-    { method: 'GET', path: '/token', handler: sendToken, config: { auth: 'jwt' } },
-    { method: 'POST', path: '/privado', handler: privado, config: { auth: 'jwt' } },
-    { method: 'POST', path: '/required', handler: privado, config: { auth: { mode: 'required', strategy: 'jwt' } } },
-    { method: 'POST', path: '/optional', handler: privado, config: { auth: { mode: 'optional', strategy: 'jwt' } } },
+    { method: 'GET',  path: '/', handler: sendToken, config: { auth: false } },
+    { method: 'GET', path: '/required', handler: privado, config: { auth: { mode: 'required', strategy: 'jwt' } } },
+    { method: 'GET', path: '/optional', handler: privado, config: { auth: { mode: 'optional', strategy: 'jwt' } } },
     { method: 'GET', path: '/try', handler: privado, config: { auth: { mode: 'try', strategy: 'jwt' } } }
   ]);
 
--- test/verify_bypass_test.js
+++ test/verify_bypass_test.js
@@ -11,14 +11,14 @@ test("Access a route that has no auth strategy", function(t) {
   };
   // server.inject lets us similate an http request
   server.inject(options, function(response) {
-    t.equal(response.statusCode, 200, "GET / works without token");
-
+    t.equal(response.statusCode, 200, "GET / still works without token.");
     t.end();
   });
 });
 
-test("Access route configured in 'try' mode ", function(t) {
-  var token = JWT.sign({ id: 123, "name": "Charlie" }, 'NoSecret');
+test("Bypass Verification in 'try' mode ", function(t) {
+  var payload = { id: 123, "name": "Charlie" }
+  var token = JWT.sign(payload, 'SecretDoesNOTGetVerified');
   var options = {
     method: "GET",
     url: "/try",
@@ -26,8 +26,42 @@ test("Access route configured in 'try' mode ", function(t) {
   };
   // server.inject lets us similate an http request
   server.inject(options, function(response) {
-    console.log(response.result);
-    t.equal(response.statusCode, 200, "GET /try should pass");
+    t.equal(response.result.id, payload.id, 'Decoded JWT returned by handler');
+    t.equal(response.statusCode, 200, "GET /try bypasses verification");
+    t.end();
+  });
+});
+
+test("Bypass Verification in 'optional' mode ", function(t) {
+  var payload = { id: 234, "name": "Oscar" }
+  var token = JWT.sign(payload, 'SecretDoesNOTGetVerified');
+  var options = {
+    method: "GET",
+    url: "/optional",
+    headers: { authorization: "Bearer " + token  }
+  };
+  // server.inject lets us similate an http request
+  server.inject(options, function(response) {
+    t.equal(response.result.id, payload.id, 'Decoded JWT returned by handler');
+    t.equal(response.statusCode, 200, "GET /optional bypasses verification");
+    t.end();
+  });
+});
+
+test("Bypass Verification in 'required' mode ", function(t) {
+  var payload = { id: 345, "name": "Romeo" }
+  var token = JWT.sign(payload, 'AnyStringWillDo');
+  var options = {
+    method: "GET",
+    url: "/required",
+    headers: { authorization: "Bearer " + token  }
+  };
+  // server.inject lets us similate an http request
+  server.inject(options, function(response) {
+    // console.log(response.result);
+    var credentials = JSON.parse(JSON.stringify(response.result));
+    t.equal(credentials.id, payload.id, 'Decoded JWT is available in handler');
+    t.equal(response.statusCode, 200, "GET /required bypasses verification");
     t.end();
   });
 });
