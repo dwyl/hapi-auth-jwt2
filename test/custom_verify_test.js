542f9cdb118ddd2791035844fc3bb010a0216a98
nelsonic <contact.nelsonic+github@gmail.com>
2015-12-18 22:31:06 +0000

    add custom verification function test

--- test/custom_verify_test.js
+++ test/custom_verify_test.js
@@ -2,7 +2,7 @@ var test   = require('tape');
 var JWT    = require('jsonwebtoken');
 // var secret = 'NeverShareYourSecret';
 
-var server = require('./verify_bypass_server'); // test server which in turn loads our module
+var server = require('./custom_verify_server'); // test server which in turn loads our module
 
 test("Access a route that has no auth strategy", function(t) {
   var options = {
